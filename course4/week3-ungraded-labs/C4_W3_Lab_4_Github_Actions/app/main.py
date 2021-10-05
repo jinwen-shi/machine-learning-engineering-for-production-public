793a2309c8056690359768b005819323f7385092
Jinwen <jshi@reptrak@users.noreply.github.com>
2021-10-05 12:28:01 -0400

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# running GitHub Actions
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
